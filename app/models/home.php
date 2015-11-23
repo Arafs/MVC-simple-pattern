794d8f12c87eb622a6600455e2b87dfb992d75e8
Araf Shihab/Laguardia Community Colllege <araf@criterionmedglobal.com>
2015-11-23 15:56:00 -0800

    Update home.php

--- app/models/home.php
+++ app/models/home.php
@@ -5,6 +5,7 @@ class Model {
 	public function __construct () {
 		echo 'Hello World'; //leads us to echo hello world onto the page.
 				//However we can pass this onto the view to do the renderation if we like.
+		
 	}
 	
 }
