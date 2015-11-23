a86c9ed2d752d2d1a3fc70ef6ec8beab2b05f50e
Araf Shihab/Laguardia Community Colllege <araf@criterionmedglobal.com>
2015-11-23 15:56:43 -0800

    Update home.php

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -4,7 +4,7 @@ class Home extends Controller {
 	public function __construct () {
 		require_once "..\app\models\\" . get_class($this) . ".php";
 		$model = new Model(); // Constructor for our controller where the application begins. 
-		$views = new Views();			//Creating an instance of our Model class.
+					//Creating an instance of our Model class, which will lead to our views
 	}
 	
 	public function index () {
